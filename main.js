450d7ad5197d4f923fb8e98ac1ae660301d96068
Mickk77 <krisztian.balazs39@freemail.hu>
2021-10-06 18:23:07 +0200

    main2

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $(".btnDelete").on('click', function(){
-        $(this).closest("tr").remove();
+        $(this).closest("tr").hide(1200);
+    });
+    $(".item").click(function(){
+        $(this).css("color", "red");
+    });
+    $(".item").dblclick(function(){
+        $(this).css("color", "blue");
+    });
+    $("p").click(function(){
+        alert("If quantity is less: one click on item => text will be red. \nIf quantity is more: double click on item => text will be blue. \nIf quantity is correct: click on the button => the row will be hide.");
+    });
+    $("h1").hover(function(){
+        $(this).css("color", "brown");
     });
 });
